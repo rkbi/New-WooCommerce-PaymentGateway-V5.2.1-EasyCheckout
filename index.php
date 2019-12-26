854922df17ae8121ac3e608da77dd445d3cca05a
Prabal Mallick <33824111+prabalsslw@users.noreply.github.com>
2019-12-26 10:11:43 +0600

    Update index.php

--- index.php
+++ index.php
@@ -231,6 +231,19 @@ function woocommerce_sslcommerz_init()
             $declineURL = $order->get_cancel_order_url();
 
             $items = $woocommerce->cart->get_cart();
+            #ship
+            $shipping_method = @array_shift($order->get_shipping_methods());
+            $shipping_method_id = $shipping_method['method_id'];
+
+            if($shipping_method_id!="")
+            {
+                $smethod = "YES";
+            }
+            else
+            {
+                $smethod = "NO";
+            }
+            //---
             $product_title = array();
 
             foreach($items as $item => $values) 
@@ -276,7 +289,7 @@ function woocommerce_sslcommerz_init()
                 'ship_postcode' => $order->shipping_postcode,
                 'currency'      => get_woocommerce_currency(),
                 'product_category'  => 'ecommerce',
-                'shipping_method'   => 'YES',
+                'shipping_method'   => $smethod,
                 'num_of_item'       => $woocommerce->cart->cart_contents_count,
                 'product_name'      => $product_name,
                 'product_profile'   => 'general',
